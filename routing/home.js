dc53e6001d5187d860e102089ed9ac7ae8f989d5
Daniel Tucker <daniel.tucker@outlook.com>
2024-06-16 17:15:56 -0700

    clearing old contents

--- routing/home.js
+++
@@ -1,9 +0,0 @@
-//const queryTools = require('./mongo');
-const { Router } = require('express');
-const app = Router();
-
-app.get('/', function(request, response) {
-    response.send('<h1>Hello World!</h1>')
-});
-
-module.exports = app;
